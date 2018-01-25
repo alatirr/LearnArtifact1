dcc44885fceefeca56b560878c2d11ef5e7972c0
alatirr <bale90@yandex.com>
2018-01-25 17:34:02 +0300

    3.1 Реализованы базовые операции калькулятора (+,-,/,*)

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -8,4 +8,14 @@ public class Arithmetic {
         float minus = num1 - num2;
         return minus;
     }
+
+    public float DivisionFraction(float num1, float num2){
+        float division = num1 / num2;
+        return division;
+    }
+
+    public float MultiplicationFraction(float num1, float num2){
+        float multiplication = num1 * num2;
+        return multiplication;
+    }
 }
--- src/main/java/InputScan.java
+++ src/main/java/InputScan.java
@@ -4,26 +4,26 @@ public class InputScan {
     public void InputScanArithmetic(){
         boolean stop = true;
         while(stop) {
-            System.out.println("Choose operation: input minus or sum");
+            System.out.println("Choose operation: input minus or sum or degree or multiple");
             Scanner sc = new Scanner(System.in);
             String inputOperation = sc.nextLine();
+            System.out.println("Enter the first number: ");
+            float num1 = sc.nextFloat();
+            System.out.println("Enter the second number: ");
+            float num2 = sc.nextFloat();
+            Arithmetic arithmetic = new Arithmetic();
+            System.out.println("Enter the first number: ");
             if(new String("minus").equals(inputOperation)) {
-                System.out.println("Enter the first number: ");
-                float num1 = sc.nextFloat();
-                System.out.println("Enter the second number: ");
-                float num2 = sc.nextFloat();
-                Arithmetic arMinus = new Arithmetic();
-                System.out.println("Enter the first number: ");
-                System.out.printf("%.4f", arMinus.MinusFraction(num1, num2));
+                System.out.printf("%.4f", arithmetic.MinusFraction(num1, num2));
             }
              else if(new String("sum").equals(inputOperation)){
-                System.out.println("Enter the first number: ");
-                float num1 = sc.nextFloat();
-                System.out.println("Enter the second number: ");
-                float num2 = sc.nextFloat();
-                Arithmetic arSum = new Arithmetic();
-                System.out.println("Enter the first number: ");
-                System.out.printf("%.4f", arSum.SumFraction(num1, num2));
+                System.out.printf("%.4f", arithmetic.SumFraction(num1, num2));
+            }
+            else if(new String("degree").equals(inputOperation)){
+                System.out.printf("%.4f", arithmetic.DivisionFraction(num1, num2));
+            }
+            else if(new String("multiple").equals(inputOperation)){
+                System.out.printf("%.4f", arithmetic.MultiplicationFraction(num1, num2));
             }
             System.out.println("You want exit? Press q");
             String exit = sc.nextLine();
