4827737ca1d3f6aefadd56e4d8dd9bbc02cce014
alatirr <bale90@yandex.com>
2018-02-19 11:10:54 +0300

    4.1. repeat

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,12 +1,13 @@
 import calculator.InputScan;
 import massiv.MaxElemet;
+import massiv.mass41;
 
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String [] args){
         int check = 0;
-        System.out.println("1 - run calculator, 2 - run massiv, 0 - exit");
+        System.out.println("1 - run calculator, 2 - run massiv, 3 - task 4.1, 0 - exit");
         Scanner sc = new Scanner(System.in);
         check = sc.nextInt();
         if(check == 1){
@@ -16,6 +17,10 @@ public class Calculator {
         else if(check == 2){
             MaxElemet maxElemet = new MaxElemet();
             maxElemet.maxElementFind();
+        }
+        else if(check == 3){
+            mass41 m41 = new mass41();
+            m41.run41();
         }
     }
 }
--- src/main/java/massiv/mass41.java
+++ src/main/java/massiv/mass41.java
@@ -1,4 +1,48 @@
 package massiv;
 
+import java.util.Random;
+/* Массив размерностью 20, заполняется случайными целыми числами от -10 до 10.
+ Найти максимальный отрицательный и минимальный положительный элементы массива. Поменять их местами. */
 public class mass41 {
+    public void run41(){
+        int mass[] = new int[20];
+        for(int i = 0; i < mass.length; i++){
+            Random rand = new Random();
+            int randomNum = rand.nextInt((10 - (-10))) - 10;
+            mass[i] = randomNum;
+        }
+        int maxOtr = -11;
+        int minPol =  11;
+        System.out.println("ishodnii massiv: " );
+        for(int i = 0; i < mass.length; i++){
+            System.out.print(" " + mass[i]);
+            if(mass[i] > 0 && mass[i] < minPol){
+                minPol = mass[i];
+            }
+            if(mass[i] < 0 && mass[i] > maxOtr){
+                maxOtr = mass[i];
+            }
+        }
+        System.out.println();
+        System.out.println("minPol elem: " + minPol );
+        System.out.println("maxOtr elem: " + maxOtr);
+        int k = 0;
+        int n = 0;
+        int m =0;
+        for(int i = 0; i < mass.length; i++){
+            if(mass[i] == minPol ){
+                k = i;
+            }
+            if(mass[i] == maxOtr){
+                n = i;
+            }
+        }
+        mass[k] = maxOtr;
+        mass[n] = minPol;
+
+        System.out.println("change array: ");
+        for(int i : mass){
+            System.out.print(i + " ");
+        }
+    }
 }
